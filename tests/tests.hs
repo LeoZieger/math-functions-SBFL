e61c884d9b685abae47b25e9f4c99675fb8cece3
Leonard Zieger <Leonard.Zieger@outlook.de>
2023-11-30 11:01:39 +0100

    Enable tasty-coverage and fhpc

--- tests/tests.hs
+++ tests/tests.hs
@@ -1,12 +1,13 @@
-import Test.Tasty       (defaultMain,testGroup)
+import Test.Tasty (defaultMain,testGroup, defaultMainWithIngredients)
 import qualified Tests.Chebyshev
 import qualified Tests.Comparison
 import qualified Tests.RootFinding
 import qualified Tests.SpecFunctions
 import qualified Tests.Sum
+import Test.Tasty.CoverageReporter
 
 main :: IO ()
-main = defaultMain $ testGroup "math-functions"
+main = defaultMainWithIngredients [coverageReporter] $ testGroup "math-functions"
   [ Tests.SpecFunctions.tests
   -- FIXME: tests for chebyshev polynomials fail intermittently
   -- , Tests.Chebyshev.tests
